a606ca37b3a5cf36b81ba0ebd050d8dc9804e844
Krzysztof K <krzysztofk7777@gmail.com>
2020-01-28 22:04:51 +0100

    Task 12

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int a = -10;
-        do {
-            a++;
-            System.out.println(a);
-        } while (a < 0);
+        int a = 10;
+        for (int i = 0; i <a ; i++) {
+            System.out.println(i);
+        }
+
 
     }
 }
