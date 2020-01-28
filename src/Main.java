e1c9de353c1246f4a29a81d44d494a31f75fc51d
Krzysztof K <krzysztofk7777@gmail.com>
2020-01-28 21:55:21 +0100

    Task 10

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        isEven(2);
-        isEven(13);
-        isEven(17);
-        isEven(36);
-
-    }
-
-    public static void isEven(Integer number) {
-
-        int nCase = number % 2;
-        switch (nCase){
-            case 0:
-                if(nCase == 0)
-                    System.out.println("Number "+number+" is even");
-                    break;
-            case 1:
-                if(nCase != 0)
-                    System.out.println("Number "+number+" is not even");
-                    break;
-
+        int a = 10;
+        while (a > 0) {
+            a--;
+            System.out.println(a);
         }
+
     }
 }
 
