b7d7e32988d5fd33a55b600a4ba24f2a3430b744
Krzysztof K <krzysztofk7777@gmail.com>
2020-01-28 22:39:45 +0100

    Task 13

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,19 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
+        method(20);
+    }
 
-        int a = 10;
-        for (int i = 0; i <a ; i++) {
-            System.out.println(i);
+    public static void method(int number) {
+        for (int i = 0; i <= number; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (i % 11 == 0) {
+                break;
+            }
+            System.out.println(i + " / " + number + " is not even");
+            System.out.println("Number 11 stops the loop!");
         }
 
 
