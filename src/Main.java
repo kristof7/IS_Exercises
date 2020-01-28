d7dc0cfe642bc0b53bca4366cee98a15d02b86fc
Krzysztof K <krzysztofk7777@gmail.com>
2020-01-28 21:00:00 +0100

    Task 8

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,21 @@ public class Main {
 
     public static void main(String [] args) {
 
-        Calculator calculator = new Calculator();
-        System.out.println("(integer)divide 20 / 10 = "+calculator.divide(20, 10));
-        System.out.println("(integer)divide 7 / 2 = "+calculator.divide(7, 2));
-        System.out.println("(integer)divide 100 / 23 = "+calculator.divide(100, 23));
+        Car car1 = new Car();
+        Car car2 = new Car();
 
+        car1.setName("car1");
+        car2.setName("car2");
+        car1.setMaxSpeed(100);
+        car2.setMaxSpeed(200);
+
+        if (car1.getMaxSpeed() > car2.getMaxSpeed()) {
+            car1.setName("faster car");
+            System.out.println("Car1 is "+ car1.getName());
+        }
+        else {
+            car2.setName("faster car");
+            System.out.println("Car2 is "+ car2.getName());
+        }
     }
 }
