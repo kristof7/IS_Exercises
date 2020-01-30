9d9ebcd1fa9cf6b015133cb9660f151ae901aed2
Krzysztof K <krzysztofk7777@gmail.com>
2020-01-30 16:57:08 +0100

    Task 16

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String newName = scanner.nextLine();
-        int newEngine = scanner.nextInt();
+        String tekstA = "tekst";
+        String tekstB = "tekst";
 
-        Engine engine = new Engine();
-        engine.setPower(newEngine);
-        Car car1 = new Car();
-        car1.setName(newName);
-        car1.setEngine(engine);
-
-        System.out.println("Car name: "+car1.getName());
-        System.out.println(car1.getName()+"'s"+" engine power: "+engine.getPower());
-
+        if (tekstA == tekstB) {
+            System.out.println("tekstA hashcode: "+tekstA.hashCode());
+            System.out.println("tekstB hashcode: "+tekstB.hashCode());
+        }
+        if (tekstA.equals(tekstB)) {
+            System.out.println("tekstA hashcode: "+tekstA.hashCode());
+            System.out.println("tekstB hashcode: "+tekstB.hashCode());
+        }
     }
 }
 
